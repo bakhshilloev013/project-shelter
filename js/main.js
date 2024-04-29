99b7ef14ef0706a7340b370ac83e75b6d7d8ed39
Bekhzod Bakhshilloev <beki2627@gmail.com>
2024-04-29 11:51:42 +0500

    slider on main page complete

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
 'use strict';
 
+// import firebase from 'firebase/app';
+// import 'firebase/database';
+
 document.addEventListener('DOMContentLoaded', function () {
     // Функционал лавной прокрутки по ссылкам в блоке навигации
     document.querySelectorAll('.header_menu li a').forEach((link) => {
@@ -21,5 +24,69 @@ document.addEventListener('DOMContentLoaded', function () {
         document.querySelector('#our_friends').scrollIntoView({
             behavior: 'smooth',
         });
+    });
+
+    const content = document.querySelector(
+            '.our_friends_slider .slider_content'
+        ),
+        prev = document.querySelector('.our_friends_slider .slider_left'),
+        next = document.querySelector('.our_friends_slider .slider_right');
+
+    let valueOfPets;
+
+    fetch('../data/pets.json')
+        .then((response) => response.json())
+        .then((json) => renderContent(json));
+
+    function renderContent(data) {
+        valueOfPets = data.length;
+        content.style.cssText = `width: ${
+            valueOfPets * 270 + (valueOfPets - 1) * 90
+        }px`;
+        content.innerHTML = '';
+        data.forEach((item, i) => {
+            content.innerHTML += `
+            <div data-index=${i} class="slider_card">
+                <img
+                    src="img/pets/pets-${i + 1}.png"
+                    alt=""
+                    class="card_img"
+                />
+                <div class="card_title">${item.name}</div>
+                <button class="card_button">
+                    Learn more
+                </button>
+            </div>
+            `;
+        });
+    }
+
+    let value = 0;
+    next.addEventListener('click', (e) => {
+        if (value === -1800) {
+            next.classList.add('slider_btn_disable');
+            return;
+        }
+        value = value - 270 - 90;
+        if (value < 0) {
+            console.log('did');
+            prev.classList.remove('slider_btn_disable');
+        }
+
+        console.log(value);
+        content.style.cssText += `transform: translateX(${value}px)`;
+    });
+
+    prev.addEventListener('click', (e) => {
+        if (value === 0) {
+            prev.classList.add('slider_btn_disable');
+            return;
+        }
+        value = value + 270 + 90;
+        if (value > -1800) {
+            next.classList.remove('slider_btn_disable');
+        }
+        console.log(value);
+        content.style.cssText += `transform: translateX(${value}px)`;
     });
 });
